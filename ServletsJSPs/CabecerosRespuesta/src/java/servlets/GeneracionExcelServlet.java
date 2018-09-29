5d0744c248ee1362b59c23d17306c5697a0f8c62
Joaquin Culasso <Joaquin.culasso@gmail.com>
2018-09-29 17:23:36 -0300

    CabecerosRespuesta

---
+++ ServletsJSPs/CabecerosRespuesta/src/java/servlets/GeneracionExcelServlet.java
@@ -0,0 +1,30 @@
+package servlets;
+
+import java.io.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+@WebServlet(name = "GeneracionExcelServlet", urlPatterns = {"/GeneracionExcelServlet"})
+public class GeneracionExcelServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        try (PrintWriter out = response.getWriter()) {
+            //Especificamos el tipo del documento de respuesta (MIME)
+            response.setContentType("aplicacion/vnd.ms-excel");
+            response.setHeader("Content-Disposition", "attachment; filename=excelEjemplo.xls");
+            //Para un uso más profesional de excel usar: http://poi.apache.org/
+            response.setHeader("Pragma", "no-cache");
+            response.setHeader("Cache-Control", "no-store");
+            response.setDateHeader("Expires", -1);
+            //Desplegamos el contenido en un excel simplemente modificando el contentType
+            out.println("\tValores");
+            out.println("\t1");
+            out.println("\t2");
+            out.println("Total\t=SUMA(B2:B3)");
+        }
+    }
+}
